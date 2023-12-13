4126a866456f50562b02a9e104ebd0d745e8fe44
Ghanu2204 <ghanshyamsojitra173@gmail.com>
2023-12-13 17:51:34 +0530

    Home page is added

---
+++ app/Service/page.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {} from "react-icons/";
+
+const Service = () => {
+  return (
+    <>
+      <div class="svice">
+        <div>
+          <h1 class="stitle">Our Services</h1>
+          <h3 class="detail">
+            Lorem lpsum is simply dummy text of the printing and typesetting
+            industry. Lorem ipsum has been the industry's standard dummy text
+            ever since the
+          </h3>
+        </div>
+        <div class="stabs">
+          <div class="tab">
+            <div class="E-icon"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Service;
